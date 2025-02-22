64cfe039d64f933dd7a8bca6da47d3fb2733535a
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-02-22 15:00:55 +0800

    Update JS8.js

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,23 @@
-// your code starts here
+const counterText = document.querySelector("#counter-text");
+const addButton = document.querySelector("#button-add");
+const subtractButton = document.querySelector("#button-subtract");
+
+let counterVal = 0;
+
+function updateCounter() {
+    counterText.textContent = counterVal;
+}
+
+addButton.addEventListener("click", function () {
+    counterVal++;
+    updateCounter();
+});
+
+subtractButton.addEventListener("click", function () {
+    if (counterVal > 0) {
+        counterVal--;
+        updateCounter();
+    }
+});
+
+updateCounter();
