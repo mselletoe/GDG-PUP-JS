2ca0218f65e51ccb9e3201b0fe9a90a7735569d3
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-02-22 15:00:51 +0800

    Update script.js

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,13 @@
+let favoriteMovies = [
+    "Herbie: Fully Loaded",
+    "An Inconvenient Love",
+    "Fast Five",
+    "Furious 7",
+    "Avengers: Age of Ultron",
+    "Iron Man 2",
+    "Fast & Furious 6"
+]
+
+for (let i = 0; i < favoriteMovies.length; i++) {
+    console.log(favoriteMovies[i]);
+}
